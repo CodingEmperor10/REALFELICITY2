9209671471cdfd5338e85341748d4aa4cc324b8b
CodingEmperor10 <85398753+CodingEmperor10@users.noreply.github.com>
2021-08-21 22:06:20 +0530

    Add files via upload

---
+++ felicity_room.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyBxcR1yjsNVRV-0hs8T9ERVtBjny_VE8MU",
+    authDomain: "felicity-f3fd0.firebaseapp.com",
+    databaseURL: "https://felicity-f3fd0-default-rtdb.firebaseio.com",
+    projectId: "felicity-f3fd0",
+    storageBucket: "felicity-f3fd0.appspot.com",
+    messagingSenderId: "528357309185",
+    appId: "1:528357309185:web:cde700a947c4407054b900"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
---
+++ main.js
@@ -0,0 +1,8 @@
+
+function addUSer(){
+happy_input = document.getElementById("happy_input").value;
+
+localStorage.setItem("happy input", happy_input);
+
+window.location = "felicity_room.html";
+}
